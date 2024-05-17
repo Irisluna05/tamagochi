ff894555255e20347a0ad936b5978c797ba33967
Irisluna05 <145460302+Irisluna05@users.noreply.github.com>
2024-05-17 18:28:25 +0200

    Versie1
    Toevoegen hungerbalk
    verandering image bij hoger en lager

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const titel = document.querySelector("h1")
-let tamagochi = document.querySelector("img")
+let tamagochi = document.querySelector("#main")
 const btn = document.querySelector ("#ketting")
+const btn2 = document.querySelector ("#eten")
+const praten = document.querySelector("#tekst")
+let hungerbalk = document.querySelector ("#hunger")
+let hunger = 0
 
-function changeImage() {document.querySelector("img").src='images/vogel.png'
+function verhoogHunger() {
+    hunger = hunger + 1
+    hungerbalk.value = hunger
+    changeImageback()
 }
 
-tamagochi.addEventListener("click", changeImage);
+setInterval (verhoogHunger,2000)
+
+function verlaagHunger() {
+    hunger = hunger - 2
+    hungerbalk.value = hunger
+    changeImage()
+}
+
+
+const pElement = document.querySelector("#tekst")
+
+function changeImage() {document.querySelector("#main").src='images/vogel.png'}
+function changeImageback() {document.querySelector("#main").src='images/vogel3.png'}
+
+
+    if (hunger.value =+ 8) {
+        praten.textContent = "Ik heb honger";
+    }
+    else if (hunger.value <= 8) {
+        praten.textContent = "Hallo";
+    }
+
+btn2.addEventListener("click",verlaagHunger);
+setTimeout (changeImageback,5000)
