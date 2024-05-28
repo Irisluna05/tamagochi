d0192004fbf8b0e30c4e2b153a2f754cdd071c8a
Irisluna05 <145460302+Irisluna05@users.noreply.github.com>
2024-05-28 20:08:11 +0200

    Audio versie
    Er zijn nu muziekjes in het document

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-const titel = document.querySelector("h1")
-let tamagochi = document.querySelector("#main")
-const btn = document.querySelector ("#ketting")
-const btn2 = document.querySelector ("#eten")
-const pElement = document.querySelector("#tekst")
+const titel = document.querySelector("h1");
+let tamagochi = document.querySelector("#main");
+const btn = document.querySelector("#ketting");
+const btn2 = document.querySelector("#eten");
+const pElement = document.querySelector("#tekst");
 
-let hungerbalk = document.querySelector ("#hunger")
-let hunger = 0
+let muziekje = new Audio("audio/cardinal.mp3"); //Bron Thomas Plas en Mozilla
+muziekje.play();
+
+let happy = new Audio("audio/happy.mp3")
+let hungerbalk = document.querySelector("#hunger");
+let hunger = 0;
 
 function verhoogHunger() {
-    hunger = hunger + 1
-    hungerbalk.value = hunger
-    changeImageback()
-    updateText()
-    checkHungerLevel()
+    if (hunger < 10) {
+        hunger += 1;
+    }
+    hungerbalk.value = hunger;
+    changeImageback();
+    updateText();
+    checkHungerLevel();
 }
 
-setInterval (verhoogHunger,2000)
+setInterval(verhoogHunger, 2000); //Bron theorie uit de les
 
 function verlaagHunger() {
-    hunger = hunger - 1
-    hungerbalk.value = hunger
-    changeImage()
-    updateText()
+    if (hunger > 0) {
+        hunger -= 1;
+        hungerbalk.value = hunger;
+        happy.play();
+        changeImage();
+        updateText();
+    }
 }
 
 function updateText() {
@@ -31,19 +40,24 @@ function updateText() {
         pElement.textContent = "Hallo";
     }
 }
- 
+
 function checkHungerLevel() {
     if (hunger >= 10) {
         tamagochi.src = 'images/vogelsad.png';
+        pElement.textContent = "Ik wil nu ETEN";
     } else {
         tamagochi.src = 'images/vogel3.png';
     }
 }
 
-function changeImage() 
-{document.querySelector("#main").src='images/vogel.png'}
-function changeImageback() 
-{document.querySelector("#main").src='images/vogel3.png'}
+function changeImage() {
+    tamagochi.src = 'images/vogel.png';
+}
 
-btn2.addEventListener("click",verlaagHunger);
-setTimeout (changeImageback,5000)
+function changeImageback() {
+    tamagochi.src = 'images/vogel3.png';
+}
+
+
+btn2.addEventListener("click", verlaagHunger);
+setTimeout(changeImageback, 5000);
